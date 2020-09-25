58e160f0d8c65f106ebec81ac0c4634c22b135b0
z2e3r40o <z2e3r40o@protonmail.com>
2020-09-24 23:39:46 -0700

    use mailgun

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,16 @@ module Example
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    host = 'rails-with-gpg.herokuapp.com'
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => host,
+      :authentication => :plain,
+    }
   end
 end
